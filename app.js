0bc38f18a492121752a25a2d2bf205ae062e307c
ravigordon <rgordz460@outlook.com>
2022-05-18 15:01:21 +0100

    foreach to loop over mouse movement created

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ const inputs = document.querySelectorAll('.controls input');
 function handleUpdate(){
 
 }
-inputs.forEach(input => input.addEventListener('change', handleUpdate))
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
